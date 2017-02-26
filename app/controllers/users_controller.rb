d3af865b4c87b213b739519e2de17cc454b277f4
frogtube <u.pradere@gmail.com>
2017-02-26 10:18:35 +0100

    Ajout du mode administrateur

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,5 @@
 class UsersController < ApplicationController
   def home
-  	if session[:user_id]
-  		@current_user= User.find(session[:user_id])
-  	end
   end
 
   def login
@@ -12,10 +9,14 @@ class UsersController < ApplicationController
   	@current_user = User.where(name: params[:name], password: params[:password]).first
   	if @current_user
   		session[:user_id] = @current_user.id
-  		flash[:info] = "Bienvenue #{@current_user.name}"
+  		if @current_user.role == "admin"
+	  		flash[:info] = "Bienvenue administrateur #{@current_user.name}"
+  		else
+	  		flash[:info] = "Bienvenue #{@current_user.name}"
+  		end
   		redirect_to "/users/home"
   	else
-  		session[:user_id] = 
+  		session[:user_id] = nil
   		flash[:info] = "Echec de la connexion"
   		redirect_to "/users/login"
   	end
