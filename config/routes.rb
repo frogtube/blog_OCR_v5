2a227f02fe0aa7fafb0160c725c64b5a3344ee27
frogtube <u.pradere@gmail.com>
2017-02-26 09:45:56 +0100

    Persistance de connexion utilisateur et deconnexion

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   get 'users/home' => 'users#home'
-
   get 'users/login' => 'users#login'
+  delete 'users/home' => 'users#logout'
   post 'users/login' => 'users#check'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
